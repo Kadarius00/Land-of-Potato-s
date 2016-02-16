4b7ac70e5e74aa59dd68a29f84632caf44053303
kadarius00 <kadarius@ymail.com>
2016-02-16 20:36:35 +0000

    first commit

---
+++ fave.rb
@@ -0,0 +1,13 @@
+print "What's your first name?"
+first_name = gets.chomp
+first_name.capitalize!
+
+print "What's your last name?"
+last_name = gets.chomp
+last_name.capitalize!
+
+print "Who's your favorite performer?"
+performer = gets.chomp
+performer.capitalize!
+
+print "My name is #{first_name} #{last_name}. My favorite performer or act is #{performer}."
